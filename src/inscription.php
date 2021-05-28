0b6a0d37d823440f1a64dbe06bce65bf7df881c7
Charlene167 <78904814+Charlene167@users.noreply.github.com>
2021-05-28 18:22:33 +0200

    update inscription image

--- src/inscription.php
+++ src/inscription.php
@@ -42,9 +42,14 @@
 </head>
 
 <body>
-	<img src="home.png" alt="home">
+<a href="#"><svg style="width:24px;height:24px" viewBox="0 0 24 24">
+    <path fill="currentColor" d="M10,20V14H14V20H19V12H22L12,3L2,12H5V20H10Z" />
+    </svg>
+    </a>
 	<div class="conteneur">
-		<img src="profil.png" alt="profil" width="100px" height="100px"><br><br>
+	<svg style="width:24px;height:24px" viewBox="0 0 24 24">
+    <path fill="currentColor" d="M12,4A4,4 0 0,1 16,8A4,4 0 0,1 12,12A4,4 0 0,1 8,8A4,4 0 0,1 12,4M12,14C16.42,14 20,15.79 20,18V20H4V18C4,15.79 7.58,14 12,14Z" />
+	</svg>
 		<form action="inscrit.php" method="post">
 			<table>
 				<tr>
