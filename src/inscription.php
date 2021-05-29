f3d6266e190f6f97b9bb194f236c116897bad5a6
Charlene167 <78904814+Charlene167@users.noreply.github.com>
2021-05-29 20:46:16 +0200

    update iscription.php

--- src/inscription.php
+++ src/inscription.php
@@ -50,7 +50,7 @@
 	<svg style="width:24px;height:24px" viewBox="0 0 24 24">
     <path fill="currentColor" d="M12,4A4,4 0 0,1 16,8A4,4 0 0,1 12,12A4,4 0 0,1 8,8A4,4 0 0,1 12,4M12,14C16.42,14 20,15.79 20,18V20H4V18C4,15.79 7.58,14 12,14Z" />
 	</svg>
-		<form action="inscrit.php" method="post">
+		<form action="./include/inscrit.inc.php" method="post">
 			<table>
 				<tr>
 					<td>Nom : </td><td><input type="text" name="nom"></td>
