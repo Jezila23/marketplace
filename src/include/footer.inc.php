cecd82948ca1c1089a2c55d3ec55c4440f6277bf
Jezila23 <78852558+Jezila23@users.noreply.github.com>
2021-05-30 10:23:28 +0200

    update footer (with Boostrap)

--- src/include/footer.inc.php
+++ src/include/footer.inc.php
@@ -1,47 +1,70 @@
+<footer class="fixed-bottom d-flex flex-wrap align-items-end justify-content-center justify-content-lg-between">
+        <div class="ms-3 col-auto footer-left">
+            <div class="row">
+                <p class="col fs-5">Nous contacter :</p>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M27.5 21.15V24.9C27.5014 25.2481 27.4301 25.5927 27.2906 25.9117C27.1511 26.2307 26.9466 26.517 26.6901 26.7523C26.4335 26.9877 26.1307 27.1669 25.8009 27.2784C25.4711 27.3899 25.1217 27.4313 24.775 27.4C20.9285 26.9821 17.2337 25.6677 13.9875 23.5625C10.9673 21.6433 8.40665 19.0827 6.48748 16.0625C4.37495 12.8015 3.06028 9.08874 2.64998 5.225C2.61874 4.87933 2.65982 4.53095 2.7706 4.20203C2.88138 3.87311 3.05944 3.57086 3.29343 3.31453C3.52743 3.05819 3.81223 2.85339 4.12972 2.71315C4.4472 2.57292 4.7904 2.50033 5.13748 2.5H8.88748C9.49411 2.49403 10.0822 2.70885 10.5422 3.10441C11.0021 3.49998 11.3026 4.04931 11.3875 4.65C11.5458 5.85008 11.8393 7.02841 12.2625 8.1625C12.4307 8.6099 12.4671 9.09614 12.3674 9.5636C12.2677 10.0311 12.0361 10.4601 11.7 10.8L10.1125 12.3875C11.8919 15.5169 14.483 18.1081 17.6125 19.8875L19.2 18.3C19.5398 17.9639 19.9689 17.7323 20.4364 17.6326C20.9038 17.5329 21.3901 17.5693 21.8375 17.7375C22.9716 18.1607 24.1499 18.4542 25.35 18.6125C25.9572 18.6982 26.5117 19.004 26.9081 19.4719C27.3046 19.9397 27.5152 20.537 27.5 21.15Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                    </svg>
+                    <a href="tel:+33144390600">01 44 39 06 00</a>
+                </div>
+            </div>
+            <div class="row d-flex flex-wrap align-items-center justify-content-start">
+                <div class="col-auto ">
+                    <svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M5 5H25C26.375 5 27.5 6.125 27.5 7.5V22.5C27.5 23.875 26.375 25 25 25H5C3.625 25 2.5 23.875 2.5 22.5V7.5C2.5 6.125 3.625 5 5 5Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                        <path d="M26.5 7.5L14 16.25L1.5 7.5" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                    </svg>
+                    <a href="mailto:ece.marketplace@ece.fr">ece.marketplace@ece.fr</a>
+                </div>
+                <div class="col-auto p-0 d-flex flew-row align-items-center justify-content-center">
+                    <svg class="me-3 mb-3" width="25" height="30" viewBox="0 0 25 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M23.5 12.5C23.5 21.25 12.25 28.75 12.25 28.75C12.25 28.75 1 21.25 1 12.5C1 9.51631 2.18526 6.65483 4.29505 4.54505C6.40483 2.43526 9.26631 1.25 12.25 1.25C15.2337 1.25 18.0952 2.43526 20.205 4.54505C22.3147 6.65483 23.5 9.51631 23.5 12.5Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                        <path d="M12.25 16.25C14.3211 16.25 16 14.5711 16 12.5C16 10.4289 14.3211 8.75 12.25 8.75C10.1789 8.75 8.5 10.4289 8.5 12.5C8.5 14.5711 10.1789 16.25 12.25 16.25Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                    </svg>
+                    <p class="mb-2">37 Quai de Grenelle, 75015 Paris</p>
+                </div>
+            </div>
+        </div>
 
-	<footer>
-		<div class="footer-left">
-			<p>
-				<b>Nous contacter :</b><br/>
-				<svg style="width:24px;height:24px" viewBox="0 0 24 24">
-    			<path fill="currentColor" d="M6.62,10.79C8.06,13.62 10.38,15.94 13.21,17.38L15.41,15.18C15.69,14.9 16.08,14.82 16.43,14.93C17.55,15.3 18.75,15.5 20,15.5A1,1 0 0,1 21,16.5V20A1,1 0 0,1 20,21A17,17 0 0,1 3,4A1,1 0 0,1 4,3H7.5A1,1 0 0,1 8.5,4C8.5,5.25 8.7,6.45 9.07,7.57C9.18,7.92 9.1,8.31 8.82,8.59L6.62,10.79Z" />
-				</svg> <a href="tel:+33144390600">01 44 39 06 00</a><br/>
-				<svg style="width:24px;height:24px" viewBox="0 0 24 24">
-    			<path fill="currentColor" d="M20,8L12,13L4,8V6L12,11L20,6M20,4H4C2.89,4 2,4.89 2,6V18A2,2 0 0,0 4,20H20A2,2 0 0,0 22,18V6C22,4.89 21.1,4 20,4Z" />
-				</svg> <a href="mailto:ece.marketplace@ece.fr">ece.marketplace@ece.fr</a><br/>
-				<svg style="width:24px;height:24px" viewBox="0 0 24 24">
-    			<path fill="currentColor" d="M12,11.5A2.5,2.5 0 0,1 9.5,9A2.5,2.5 0 0,1 12,6.5A2.5,2.5 0 0,1 14.5,9A2.5,2.5 0 0,1 12,11.5M12,2A7,7 0 0,0 5,9C5,14.25 12,22 12,22C12,22 19,14.25 19,9A7,7 0 0,0 12,2Z" />
-				</svg> 37 Quai de Grenelle, 75015 Paris
-			</p>
-		</div>
-		
-		<div class="footer-middle">
-			Copyright 2021 | Droit d’auteur ece.marketplace@ece.fr
-		</div>
+        <div class="mb-3 footer-middle">
+            Copyright 2021 | Droit d’auteur ece.marketplace@ece.fr
+        </div>
 
-		<div class="footer-right">
-			<p>
-				<a href="quisommesnous.php">Qui sommes-nous ?</a><br/><br/>
-				<a href="#"><svg style="width:24px;height:24px" viewBox="0 0 24 24">
-    			<path fill="currentColor" d="M12 2.04C6.5 2.04 2 6.53 2 12.06C2 17.06 5.66 21.21 10.44 21.96V14.96H7.9V12.06H10.44V9.85C10.44 7.34 11.93 5.96 14.22 5.96C15.31 5.96 16.45 6.15 16.45 6.15V8.62H15.19C13.95 8.62 13.56 9.39 13.56 10.18V12.06H16.34L15.89 14.96H13.56V21.96A10 10 0 0 0 22 12.06C22 6.53 17.5 2.04 12 2.04Z" />
-				</svg>
-				</a>
-				<a href="#"><svg style="width:24px;height:24px" viewBox="0 0 24 24">
-    			<path fill="currentColor" d="M7.8,2H16.2C19.4,2 22,4.6 22,7.8V16.2A5.8,5.8 0 0,1 16.2,22H7.8C4.6,22 2,19.4 2,16.2V7.8A5.8,5.8 0 0,1 7.8,2M7.6,4A3.6,3.6 0 0,0 4,7.6V16.4C4,18.39 5.61,20 7.6,20H16.4A3.6,3.6 0 0,0 20,16.4V7.6C20,5.61 18.39,4 16.4,4H7.6M17.25,5.5A1.25,1.25 0 0,1 18.5,6.75A1.25,1.25 0 0,1 17.25,8A1.25,1.25 0 0,1 16,6.75A1.25,1.25 0 0,1 17.25,5.5M12,7A5,5 0 0,1 17,12A5,5 0 0,1 12,17A5,5 0 0,1 7,12A5,5 0 0,1 12,7M12,9A3,3 0 0,0 9,12A3,3 0 0,0 12,15A3,3 0 0,0 15,12A3,3 0 0,0 12,9Z" />
-				</svg>
-				</a>
-				<a href="#"><svg style="width:24px;height:24px" viewBox="0 0 24 24">
-    			<path fill="currentColor" d="M22.46,6C21.69,6.35 20.86,6.58 20,6.69C20.88,6.16 21.56,5.32 21.88,4.31C21.05,4.81 20.13,5.16 19.16,5.36C18.37,4.5 17.26,4 16,4C13.65,4 11.73,5.92 11.73,8.29C11.73,8.63 11.77,8.96 11.84,9.27C8.28,9.09 5.11,7.38 3,4.79C2.63,5.42 2.42,6.16 2.42,6.94C2.42,8.43 3.17,9.75 4.33,10.5C3.62,10.5 2.96,10.3 2.38,10C2.38,10 2.38,10 2.38,10.03C2.38,12.11 3.86,13.85 5.82,14.24C5.46,14.34 5.08,14.39 4.69,14.39C4.42,14.39 4.15,14.36 3.89,14.31C4.43,16 6,17.26 7.89,17.29C6.43,18.45 4.58,19.13 2.56,19.13C2.22,19.13 1.88,19.11 1.54,19.07C3.44,20.29 5.7,21 8.12,21C16,21 20.33,14.46 20.33,8.79C20.33,8.6 20.33,8.42 20.32,8.23C21.16,7.63 21.88,6.87 22.46,6Z" />
-				</svg>
-				</a>
-			</p>
-		</div>
-	</footer>
+        <div class="footer-right me-3 d-flex flex-column align-items-center justify-content">
+            <div class="row mb-3">
+                <a class="fs-5" href="quisommesnous.php">Qui sommes-nous ?</a>
+            </div>
+            <div class="row mb-4">
+                <div class="col">
+                    <a href="#">
+                        <svg width="16" height="27" viewBox="0 0 16 27" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M14.75 1H11C9.3424 1 7.75269 1.65848 6.58058 2.83058C5.40848 4.00269 4.75 5.5924 4.75 7.25V11H1V16H4.75V26H9.75V16H13.5L14.75 11H9.75V7.25C9.75 6.91848 9.8817 6.60054 10.1161 6.36612C10.3505 6.1317 10.6685 6 11 6H14.75V1Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                        </svg>
+                    </a>
+                </div>
+                <div class="col">
+                    <a href="#">
+                        <svg width="28" height="27" viewBox="0 0 28 27" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M20.5 1H8C4.54822 1 1.75 3.79822 1.75 7.25V19.75C1.75 23.2018 4.54822 26 8 26H20.5C23.9518 26 26.75 23.2018 26.75 19.75V7.25C26.75 3.79822 23.9518 1 20.5 1Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                            <path d="M18.25 12.7125C18.4043 13.7528 18.2266 14.8153 17.7422 15.7488C17.2578 16.6823 16.4914 17.4393 15.552 17.9121C14.6126 18.3849 13.5481 18.5495 12.5097 18.3824C11.4714 18.2153 10.5122 17.7251 9.76855 16.9815C9.0249 16.2378 8.53467 15.2786 8.36758 14.2403C8.2005 13.202 8.36508 12.1374 8.83791 11.198C9.31073 10.2586 10.0677 9.49219 11.0012 9.00781C11.9347 8.52344 12.9972 8.34575 14.0375 8.50001C15.0987 8.65737 16.0811 9.15184 16.8396 9.91039C17.5982 10.6689 18.0926 11.6514 18.25 12.7125Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                            <path d="M21.125 6.625H21.1375" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                        </svg>
+                    </a>
+                </div>
+                <div class="col">
+                    <a href="#">
+                        <svg width="30" height="25" viewBox="0 0 30 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M29 1.25C27.803 2.09434 26.4776 2.74013 25.075 3.1625C24.3222 2.29688 23.3217 1.68336 22.2088 1.4049C21.0959 1.12645 19.9244 1.19649 18.8526 1.60556C17.7808 2.01464 16.8606 2.743 16.2162 3.69214C15.5719 4.64129 15.2346 5.76542 15.25 6.9125V8.1625C13.0533 8.21946 10.8766 7.73226 8.91376 6.7443C6.95093 5.75635 5.2629 4.29829 4 2.5C4 2.5 -1 13.75 10.25 18.75C7.67566 20.4975 4.60895 21.3737 1.5 21.25C12.75 27.5 26.5 21.25 26.5 6.875C26.4988 6.52681 26.4654 6.17949 26.4 5.8375C27.6758 4.57936 28.576 2.99089 29 1.25V1.25Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                        </svg>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </footer>
 
-	<?php 
-		if(isset($boostrap)){
-			echo "<script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js\" integrity=\"sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4\" crossorigin=\"anonymous\"></script>";
-		}
-	?>
 </body>
+
 </html>
